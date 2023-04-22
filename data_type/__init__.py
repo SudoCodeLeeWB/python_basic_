025d901a7c4969018493e8a52018b77fa5a4b196
SudoCodeLeeWB <84519146+SudoCodeLeeWB@users.noreply.github.com>
2023-04-22 07:09:30 +0000

    finished navigating system using dictionary

---
+++ data_type/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+from .Py_bool import Py_bool
+from .Py_dictionary import Py_dictionary
+from .Py_first import Py_first
+from .Py_list import Py_list
+from .Py_set import Py_set
+from .Py_string import Py_string
+from .Py_tuple import Py_tuple
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Every module in the files must include start() function, which includes every element in the file. (to show the file output)
+
+when new element is added, it needs to be included in main.py as import statement. 
+
